9a9cc71127db2815185e2de7b6d10bfc6cf9fd38
Ian Finch <ianfinch@users.noreply.github.com>
2021-06-13 13:03:20 +0100

    Refactored rest-services/rest.js to move API response logic to their own functions

--- rest-services/rest.js
+++ rest-services/rest.js
@@ -4,6 +4,18 @@ import { taffy as TAFFY } from "taffydb";
 import axios from "axios";
 
 /**
+ * Set up server
+ */
+const port = 3000;
+const server = `http://localhost:${port}`;
+const logFormat = ":date[iso] :status   [" + process.env["HOSTNAME"] + "] :method :url HTTP/:http-version :res[content-length]";
+
+/**
+ * Placeholder for database
+ */
+const db = { data: null, lastUpdated: null, errorMessage: null };
+
+/**
  * Enrich log messages with timestamp and host
  */
 const log = {
@@ -19,21 +31,6 @@ process.on("SIGINT", () => {
     log.info("Server received interrupt signal");
     process.exit();
 });
-
-/**
- * Set up server
- */
-const port = 3000;
-const server = `http://localhost:${port}`;
-const app = express();
-app.use(morgan(":date[iso] :status   [" + process.env["HOSTNAME"] + "] :method :url HTTP/:http-version :res[content-length]"));
-
-// Placeholder for database
-const db = {
-    data: null,
-    lastUpdated: null,
-    errorMessage: null
-};
 
 /**
  * Remove internal fields (starting with underscores) from an object
@@ -91,69 +88,109 @@ const dbQuery = (query) => {
 /**
  * Poll the test data service until we get some data
  */
-const pollTestData = (collection) => {
+const populateTestData = (collection) => {
 
     initialiseData(collection);
     if (!db.data) {
         setTimeout(() => {
-            pollTestData(collection);
+            populateTestData(collection);
         }, 3000);
     }
 };
 
 /**
+ * Handle a request for all items in the collection
+ */
+const getAllItems = (collection, req, res) => {
+    const queryResult = dbQuery();
+    res.status(queryResult.status).send(queryResult.data);
+};
+
+/**
+ * Handle a request for one or more specific items from a collection
+ *
+ * The format of the URL should be /<collection>/<item id>.  If the item id
+ * contains one or more commas, the server will treat the request as a list of
+ * resources to retrieve and return each one (or try to).
+ *
+ * If only one item id is passed, the object representing that item will be
+ * returned.  If a list of items is passed, then an array of objects will be
+ * returned.  Note that a request for a single item can be forced to return a
+ * list, if a comma is included in the request (i.e. /<collection>/,<item id>
+ * or /<collection>/<item id>,).
+ */
+const getSpecifiedItems = (collection, req, res) => {
+    const pk = collection.replace(/s$/, "Id");
+    const queryResult = dbQuery({[pk]: req.params.id.split(",")});
+
+    if (queryResult.data.length === 0) {
+        res.sendStatus(404);
+    } else {
+        if (req.params.id.includes(",")) {
+            res.status(queryResult.status).send(queryResult.data);
+        } else {
+            res.status(queryResult.status).send(queryResult.data[0]);
+        }
+    }
+};
+
+/**
+ * Return a list of items related to a specific item (or items) in a collection
+ *
+ * URL format should be /<collection>/<item id>/<name of related collection>.
+ *
+ * Note that the item id can be a comma-separated list, in which case a list of
+ * items related to all the items from the comma-separated list will be
+ * returned.
+ */
+const getRelatedItems = (collection, req, res) => {
+    const pk = collection.replace(/s$/, "Id");
+    const queryResult = db.data({[pk]: req.params.id}).map(x => x);
+
+    if (queryResult.length === 0) {
+        res.sendStatus(404);
+        return;
+    }
+
+    const fk = "_" + req.params.related.replace(/s$/, "Ids");
+    const subQueryResult = queryResult[0][fk];
+    if (!subQueryResult) {
+        res.sendStatus(404);
+        return;
+    }
+
+    res.status(200).send(subQueryResult);
+};
+
+
+/**
  * Create and start our server
+ *
+ * 1) Make API call to testdata service to populate the data
+ * 2) Create app server and initiate logging
+ * 3) Add routes
+ * 4) Start server
+ *
  */
 const restServer = (collection) => {
 
-    // Populate the data
-    pollTestData(collection);
+    populateTestData(collection);
 
-    // Handle the top-level request to our server
+    const app = express();
+    app.use(morgan(logFormat));
+
     app.get("/" + collection, (req, res) => {
-        const queryResult = dbQuery();
-        res.status(queryResult.status).send(queryResult.data);
+        getAllItems(collection, req, res);
     });
 
-    // Handle a request for a specific item from a collection
     app.get("/" + collection + "/:id", (req, res) => {
-
-        const pk = collection.replace(/s$/, "Id");
-        const queryResult = dbQuery({[pk]: req.params.id.split(",")});
-
-        if (queryResult.data.length === 0) {
-            res.sendStatus(404);
-        } else {
-            if (req.params.id.includes(",")) {
-                res.status(queryResult.status).send(queryResult.data);
-            } else {
-                res.status(queryResult.status).send(queryResult.data[0]);
-            }
-        }
+        getSpecifiedItems(collection, req, res);
     });
 
-    // Handle a request for related resources
     app.get("/" + collection + "/:id/:related", (req, res) => {
-
-        const pk = collection.replace(/s$/, "Id");
-        const queryResult = db.data({[pk]: req.params.id}).map(x => x);
-
-        if (queryResult.length === 0) {
-            res.sendStatus(404);
-            return;
-        }
-
-        const fk = "_" + req.params.related.replace(/s$/, "Ids");
-        const subQueryResult = queryResult[0][fk];
-        if (!subQueryResult) {
-            res.sendStatus(404);
-            return;
-        }
-
-        res.status(200).send(subQueryResult);
+        getRelatedItems(collection, req, res);
     });
 
-    // Start the server
     app.listen(port, () => {
         log.info("Server listening at " + server);
     });
